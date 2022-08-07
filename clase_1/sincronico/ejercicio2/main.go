82b63f23d97aa1539ccd3f8198645465cb48e2e5
Diego Fernando Díaz Aristiźabal <newton_maxwell@hotmail.com>
2022-08-06 20:46:44 -0500

    REFACT: Reorganización
    Organizo los ejercicios de clase 1 y clase 2

--- clase_1/sincronico/ejercicio2/main.go
+++ clase_1/sincronico/ejercicio2/main.go
@@ -1,48 +1,45 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
+
+// Realizar una aplicación que contenda una variable con el número del mes
+// Según el número, imprimir el mes al que corresponda en texto
 
 func main() {
 
-	str := "hola mundo"
-	letters, count := Count2(str)
+	month := 4
 
-	fmt.Printf("Longitud: %v conteo: %v \n", len(letters), count)
-
-	Print(letters)
-
+	fmt.Println("Mes:", Month(month))
 }
 
-// Opción 1
-func Count(word string) (letters []string, count int) {
-	letters = strings.Split(word, "")
-	count = len(letters)
-	fmt.Printf("LEN: %v ", count)
-	return
-}
+func Month(month int) (monthName string) {
 
-// Opción 2
-func Count2(word string) (letters []string, count int) {
-
-	i := 0
-	for k, v := range word {
-		fmt.Printf("%v ", string(v))
-		letters = append(letters, fmt.Sprintf("%s", string(v)))
-		i = k
+	switch month {
+	case 1:
+		monthName = "Enero"
+	case 2:
+		monthName = "Febrero"
+	case 3:
+		monthName = "Marzo"
+	case 4:
+		monthName = "Abril"
+	case 5:
+		monthName = "Mayo"
+	case 6:
+		monthName = "Junio"
+	case 7:
+		monthName = "Julio"
+	case 8:
+		monthName = "Agosto"
+	case 9:
+		monthName = "Septiembre"
+	case 10:
+		monthName = "Octubre"
+	case 11:
+		monthName = "Noviembre"
+	case 12:
+		monthName = "Diciembre"
 	}
 
-	count = i + 1
 	return
-}
-
-func Print(letters []string) {
-
-	count := 0
-	for i := 0; i < len(letters); i++ {
-		count++
-		fmt.Println(letters[i])
-	}
 }
